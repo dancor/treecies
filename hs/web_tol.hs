a0c3ba59ceb3cb2d5fa041acdae5b8f48f932b0a
Daniel Corson <danl@alum.mit.edu>
2012-12-22 22:47:28 -0500

    switch to not grabbing counts (that is too slow)

--- hs/web_tol.hs
+++ hs/web_tol.hs
@@ -2,76 +2,169 @@
 
 import Network.HTTP
 import qualified Data.Serialize as Ser
+import qualified Text.XML.Light as X
 
-type TOL = Forest (String, Maybe Int)
+data TolNode = TolNode
+    { tnId    :: Int
+    , tnName  :: String
+    , tnCount :: Maybe Int
+    } deriving (Show, Generic)
+
+instance Ser.Serialize TolNode where
+    get = deriveGet
+    put = derivePut
+
+type Fol = Forest TolNode
+
+type Tol = Tree TolNode
 
 type Taxon = [String]
 
-kingdoms = [
-  "Animalia", 
-  "Plantae",
-  "Fungi",
-  "Protozoa",
-  "Bacteria",
-  "Chromista",
-  "Viruses",
-  "Archaea"]
+type SearchInfo = ((Int, String), [(Int, String)])
 
-taxonLevels = ["kingdom", "phylum", "class", "order", "family", "genus"]
+kingdoms :: [String]
+kingdoms =
+    [ "Animalia"
+    , "Plantae"
+    , "Fungi"
+    , "Protozoa"
+    , "Bacteria"
+    , "Chromista"
+    , "Viruses"
+    , "Archaea"
+    ]
 
-childrenOfTaxon :: Taxon -> [String]
-childrenOfTaxon [] =
-  ["Animalia"]
-childrenOfTaxon ["Animalia"] = ["Acanthocephala"]
-childrenOfTaxon _ = []
+taxonLevels :: [String]
+taxonLevels =
+    [ "Kingdom"
+    , "Phylum"
+    , "Class"
+    , "Order"
+    , "Family"
+    , "Genus"
+    ]
 
+openURL :: String -> IO String
 openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 
-taxonToSearchUrl :: Taxon -> String
-taxonToSearchUrl taxon =
-  "http://www.catalogueoflife.org/col/browse/classification/" ++
-  intercalate "/" (zipWith (\ n t -> n ++ "/" ++ t) taxonLevels taxon) ++
-  "/match/1"
+idToInfo :: Int -> IO SearchInfo
+idToInfo id = searchToInfo Nothing $ "id=" ++ show id
 
-taxonToSpeciesCount :: [String] -> IO Int
-taxonToSpeciesCount [] = return 1315904
-taxonToSpeciesCount taxon = do
-  ls <- lines <$> openURL (taxonToSearchUrl taxon)
-  let 
-    line = head . drop 1 $ dropWhile (not . (isPrefixOf "Records found:")) ls
-    word = head . drop 1 $ words line
-    num = read . filter (/= ',') $ drop 1 word
-  return num
+urlize :: String -> String
+urlize = replace " " "%s"
 
-{-
-doTaxon :: Taxon -> String -> IO (Tree (Taxon, Int))
-doTaxon parent taxon = do
-  count <- taxonToSpeciesCount taxon
-  rec <- forM (childrenOfTaxon taxon) $ \ c -> do
-    let
-      taxon' = taxon ++ [c]
-    doTaxon taxon'
-  return $ Node (taxon, count) rec
--}
+topNameToInfo :: String -> IO SearchInfo
+topNameToInfo name = searchToInfo (Just "Kingdom") $ "name=" ++ urlize name
 
-fillTOL :: Taxon -> Int -> TOL -> IO TOL
-fillTOL _ 0 tol = return tol
-fillTOL parent toLevel subTrees = forM subTrees $ \ (Node (s, mI) tol) -> do
-  mI' <- case mI of
-    Nothing -> Just <$> taxonToSpeciesCount (parent ++ [s])
-    _ -> return mI
-  tol' <- case tol of
-    [] -> 
-      "http://www.catalogueoflife.org/col/webservice?name=" ++
-      ? ++ "&response=full"
-    _ -> 
-  return $ Node (s, mI') tol'
+pullCdData :: X.Element -> String
+pullCdData = X.cdData . head . X.onlyText . X.elContent
 
+pullCdDataOf :: String -> X.Element -> String
+pullCdDataOf a = pullCdData . fromJust . X.findChild (X.unqual a)
+
+grabIdAndName :: X.Element -> (Int, String)
+grabIdAndName a = (read $ pullCdDataOf "id" a, pullCdDataOf "name" a)
+
+searchToInfo :: Maybe String -> String -> IO SearchInfo
+searchToInfo rankMb search = do
+    xml <- X.parseXML <$> openURL (
+        "http://www.catalogueoflife.org/col/webservice?response=full&" ++
+        search)
+    let results = X.onlyElems . X.elContent . (!! 1) $ X.onlyElems xml
+        resultToInfo r =
+           (rank, (idAndName, kids))
+          where
+            idAndName = grabIdAndName r
+            rank = pullCdDataOf "rank" r
+            kids =
+                map grabIdAndName . X.findChildren (X.unqual "taxon") .
+                fromJust $ X.findChild (X.unqual "child_taxa") r
+        resultFilter = maybe id (\ r -> filter ((r ==) . fst)) rankMb
+    return . snd . head . resultFilter $ map resultToInfo results
+
+idAndNameToNode :: Int -> String -> TolNode
+idAndNameToNode id name = TolNode id name Nothing
+
+nodeToTree :: a -> Tree a
+nodeToTree a = Node a []
+
+searchInfoToTol :: SearchInfo -> Tol
+searchInfoToTol ((id, name), kids) =
+    Node (idAndNameToNode id name) $
+        map (nodeToTree . uncurry idAndNameToNode) kids
+
+idToTol :: Int -> IO Tol
+idToTol id = searchInfoToTol <$> idToInfo id
+
+initialFol :: IO Fol
+initialFol = map searchInfoToTol <$> mapM topNameToInfo kingdoms
+
+showFol =
+    putStr . unlines . map head . chunksOf 2 .
+    lines . drawForest . map (fmap tnName)
+
+growTol :: Int -> Tol -> IO Tol
+growTol 0 tol = return tol
+growTol toLevel (Node a []) = do
+    print a
+    idToTol $ tnId a
+growTol toLevel (Node a fol) = Node a <$> growFol (toLevel - 1) fol
+
+growFol :: Int -> Fol -> IO Fol
+growFol toLevel = mapM (growTol toLevel)
+
+main :: IO ()
 main = do
-  --let startTree = [Node (k, Nothing) [] | k <- kingdoms]
-  --print (startTree :: TOL)
-  --BS.writeFile "tol" $ Ser.encode tree'
-  startTree <- fromRight . Ser.decode <$> BS.readFile "tol"
-  tree' <- fillTOL [] 1 startTree
-  print tree'
-  --BS.writeFile "tol" $ Ser.encode tree'
+    [folF] <- getArgs
+    unlessM (doesFileExist folF) $ do
+        fol <- initialFol
+        BS.writeFile folF $ Ser.encode fol
+    fol <- fromRight . Ser.decode <$> BS.readFile folF
+    showFol fol
+    fol' <- growFol 2 fol
+    BS.writeFile folF $ Ser.encode fol'
+
+-- taxonToSearchUrl :: Taxon -> String
+-- taxonToSearchUrl taxon =
+--   "http://www.catalogueoflife.org/col/browse/classification/" ++
+--   intercalate "/" (zipWith (\ n t -> urlize n ++ "/" ++ urlize t)
+--   taxonLevels taxon) ++
+--   "/match/1"
+--
+-- taxonToSpeciesCount :: [String] -> IO Int
+-- taxonToSpeciesCount [] = error "Total species count requested."
+-- taxonToSpeciesCount taxon = do
+--   ls <- lines <$> openURL (taxonToSearchUrl taxon)
+--   let
+--     line = head . drop 1 $ dropWhile (not . (isPrefixOf "Records found:")) ls
+--     word = head . drop 1 $ words line
+--     num = read . filter (/= ',') $ drop 1 word
+--   return num
+--
+-- growTol :: Taxon -> Int -> Tol -> IO Tol
+-- growTol _ 0 tol = return tol
+-- growTol parent toLevel subTrees = forM subTrees $ \ (Node (s, mI) tol) -> do
+--   tol' <- case tol of
+--     [] -> do
+--       newKids <-
+--         map (X.cdData . head . X.onlyText . X.elContent . fromJust .
+--           X.findChild (X.unqual "name")) .
+--         X.findChildren (X.unqual "taxon") .
+--         fromJust . X.findChild (X.unqual "child_taxa") .
+--         head . X.onlyElems . X.elContent . (!! 1) . X.onlyElems .
+--         X.parseXML <$> openURL (
+--         "http://www.catalogueoflife.org/col/webservice?name=" ++ urlize s ++
+--         "&response=full")
+--       return [Node (x, Nothing) [] | x <- newKids]
+--     _ -> return tol
+--   Node (s, mI) <$> fillNodes parent toLevel tol'
+--
+-- fillNodes :: Taxon -> Int -> Tol -> [(IO a, a -> Tol -> Tol)]
+-- fillNodes parent toLevel subTrees = do
+--   r <- forM subTrees $ \ (Node (s, mI) tol) -> do
+--     let parent' = parent ++ [s]
+--     mI' <- case mI of
+--       Nothing -> Just <$> taxonToSpeciesCount parent'
+--       _ -> return mI
+--     return $ Node (s, mI') tol
+--   growTol parent (toLevel - 1) r
